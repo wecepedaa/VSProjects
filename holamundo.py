a46e021b3cd6e3f255b1fee394e9f5bc5b1af2ab
William Cepeda <wecepedaa@gmail.com>
2024-08-10 07:43:40 -0700

    Data types

---
+++ holamundo.py
@@ -0,0 +1,19 @@
+print("Hola William Xepeda")
+
+f=4.0
+g=12
+print(f/g)
+
+print(True  + False)
+
+fnombre="William"
+fapellido = "Xepeda"
+print(fnombre + " " + fapellido)
+
+full_name = f"Nombre: {fnombre} Apellido: {fapellido}"
+print(full_name)
+
+print("Will" in full_name)
+print("Will" not in full_name)
+
+
--- test.py
+++ test.py
@@ -4,4 +4,8 @@ import sys
 msg = "Hello William Xepeda!"
 print(msg)
 print(msg.lower())
-print("Published to a remote git repository as private")
+print("Published  remote git repository as private")
+a=10
+b=23
+c=a*b
+print("Respuesta:",c)
